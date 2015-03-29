bf75a75a7b33082411fbc2dfba60a0ba730875cb
Erik-K <enk@bu.edu>
2015-03-29 12:06:47 -0400

    Now Im goign to implement the menu in transfer.m

--- transfermenu.m
+++ transfermenu.m
@@ -1,14 +1,25 @@
+% This function uses an interactive menu in octave to let the user build a struct which contains the 
+% DC value of a transfer function, the zeros, and poles. This result is used by transfer.m to create
+% a transfer function for graphing and analysis.
+% The resulting struct looks like:
+% values.DC 		Scalar
+% values.zeros[]	Vector
+% values.poles[]	Vector
+
 function values = transfermenu
 	values = struct("DC", [], "zeros", [], "poles", []);
-	choice = menu("Menu with sub inputs", "Input DC gain", "Input zeros", "Input poles")
-	if (choice == 1)
-		A0 = input("Enter DC gain as one number: ")
-		values.DC(1) = A0;
-	elseif (choice == 2)
-		numerator = input("Enter the zeros as a vector, including [ ]: ")
-		values.zeros = numerator;
-	elseif (choice == 3)
-		denominator = input("Enter the poles as a vector, including [ ]: ")
-		values.poles = denominator;
-	endif
+	while (choice = menu("Menu with sub inputs", "Input DC gain", "Input zeros", "Input poles", "Finished"))
+		if (choice == 1)
+			A0 = input("Enter DC gain as one number: ")
+			values.DC(1) = A0;
+		elseif (choice == 2)
+			numerator = input("Enter the zeros as a vector, including [ ]: ")
+			values.zeros = numerator;
+		elseif (choice == 3)
+			denominator = input("Enter the poles as a vector, including [ ]: ")
+			values.poles = denominator;
+		else
+			break;
+		endif
+	endwhile
 endfunction
